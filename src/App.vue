d8b38677d2158e517d2d78db8d8266a94c0ed04b
ygr99 <196156709@qq.com>
2023-12-15 17:49:22 +0800

    从data，computed中读取数据

--- src/App.vue
+++ src/App.vue
@@ -3,83 +3,79 @@
 </template>
 
 <script setup>
-import { use } from 'echarts/core'
-import { LineChart } from 'echarts/charts'
-import {
-  TitleComponent,
-  TooltipComponent,
-  LegendComponent,
-  ToolboxComponent,
-  GridComponent
-} from 'echarts/components'
-import { CanvasRenderer } from 'echarts/renderers'
+import { ref, computed } from "vue";
 
-use([
-  TitleComponent,
-  TooltipComponent,
-  LegendComponent,
-  ToolboxComponent,
-  GridComponent,
-  LineChart,
-  CanvasRenderer
-])
+const data = ref([
+  { age: '0', event: '出生' },
+  { age: '6', event: '小学' },
+  { age: '12', event: '中学' },
+  { age: '15', event: '高中' },
+  { age: '18', event: '大学' },
+  { age: '21', event: '大四' },
+]);
 
+const option = computed(() => {
+  const xAxisData = data.value.map(d => d.event);
 
+  const meData = data.value.map(d => Number(d.age));
+  const fatherData = [29, 35, 41, 44, 47, 50];
+  const motherData = [32, 38, 44, 47, 50, 53];
+  const sisterData = [6, 12, 18, 21, 24, 27];
 
-
-
-const option = {
-  title: {
-    text: '一家人的年龄历程'
-  },
-  tooltip: {
-    trigger: 'axis'
-  },
-  legend: {
-    data: ['me', 'father', 'mother', 'sister']
-  },
-  grid: {
-    left: '3%',
-    right: '4%',
-    bottom: '3%',
-    containLabel: true
-  },
-  toolbox: {
-    feature: {
-      saveAsImage: {}
-    }
-  },
-  xAxis: {
-    type: 'category',
-    boundaryGap: false,
-    data: ['出生','小学', '中学', '高中', '大学', '大四']
-  },
-  yAxis: {
-    type: 'value'
-  },
-  series: [
-    {
-      name: 'me',
-      type: 'line',
-      data: [0, 6, 12, 15, 18, 21]
+  return {
+    title: {
+      text: '一家人的年龄历程',
     },
-    {
-      name: 'father',
-      type: 'line',
-      data: [29,35,41,44,47,50 ]
+    tooltip: {
+      trigger: 'axis',
     },
-    {
-      name: 'mother',
-      type: 'line',
-      data: [32,38,44,47,50,53]
+    legend: {
+      data: ['me', 'father', 'mother', 'sister'],
     },
-    {
-      name: 'sister',
-      type: 'line',
-      data: [6,12,18,21,24,27]
-    }
-  ]
-};
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true,
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {},
+      },
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: xAxisData,
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        name: 'me',
+        type: 'line',
+        data: meData,
+      },
+      {
+        name: 'father',
+        type: 'line',
+        data: fatherData,
+      },
+      {
+        name: 'mother',
+        type: 'line',
+        data: motherData,
+      },
+      {
+        name: 'sister',
+        type: 'line',
+        data: sisterData,
+      },
+    ],
+  };
+});
+
 </script>
 
 <style scoped>
