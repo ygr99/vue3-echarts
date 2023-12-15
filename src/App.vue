6afe167f3b694543e4e3d6afb35b62670df4b7b7
ygr99 <196156709@qq.com>
2023-12-15 17:57:15 +0800

    表单动态加载数据

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <v-chart class="chart" :option="option" />
+  <div>
+    <div>
+      <label for="event">Event:</label>
+      <input v-model="event" id="event" />
+    </div>
+    <div>
+      <label for="meAge">Me Age:</label>
+      <input v-model="meAge" id="meAge" />
+    </div>
+    <div>
+      <label for="fatherAge">Father Age:</label>
+      <input v-model="fatherAge" id="fatherAge" />
+    </div>
+    <div>
+      <label for="motherAge">Mother Age:</label>
+      <input v-model="motherAge" id="motherAge" />
+    </div>
+    <div>
+      <label for="sisterAge">Sister Age:</label>
+      <input v-model="sisterAge" id="sisterAge" />
+    </div>
+    <button @click="addData">Add Data</button>
+
+    <v-chart class="chart" :option="option" />
+  </div>
 </template>
 
 <script setup>
 import { ref, computed } from "vue";
 
-const data = ref([
-  { age: '0', event: '出生' },
-  { age: '6', event: '小学' },
-  { age: '12', event: '中学' },
-  { age: '15', event: '高中' },
-  { age: '18', event: '大学' },
-  { age: '21', event: '大四' },
-]);
+const data = ref([]);
+
+const event = ref("");
+const meAge = ref("");
+const fatherAge = ref("");
+const motherAge = ref("");
+const sisterAge = ref("");
+
+const addData = () => {
+  data.value.push({
+    event: event.value,
+    me: Number(meAge.value),
+    father: Number(fatherAge.value),
+    mother: Number(motherAge.value),
+    sister: Number(sisterAge.value),
+  });
+
+  // 清空输入框
+  event.value = "";
+  meAge.value = "";
+  fatherAge.value = "";
+  motherAge.value = "";
+  sisterAge.value = "";
+};
 
 const option = computed(() => {
-  const xAxisData = data.value.map(d => d.event);
+  const xAxisData = data.value.map((d) => d.event);
 
-  const meData = data.value.map(d => Number(d.age));
-  const fatherData = [29, 35, 41, 44, 47, 50];
-  const motherData = [32, 38, 44, 47, 50, 53];
-  const sisterData = [6, 12, 18, 21, 24, 27];
+  const meData = data.value.map((d) => d.me);
+  const fatherData = data.value.map((d) => d.father);
+  const motherData = data.value.map((d) => d.mother);
+  const sisterData = data.value.map((d) => d.sister);
 
   return {
     title: {
@@ -84,3 +124,4 @@ const option = computed(() => {
   margin-top: 100px;
 }
 </style>
+5
